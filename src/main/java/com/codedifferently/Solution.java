2aa81def257927853ad1f6a486b94a167d304df5
Khalil Brewington <kbrewing02@gmail.com>
2022-07-26 09:20:52 -0400

    formatting issue

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,19 @@
 package com.codedifferently;
 
+import java.util.HashMap;
+
 public class Solution {
-    public String howManyLettersDoYouSee(String input){
-        return null;
+    public String howManyLettersDoYouSee(String input) {
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        char[] stringArray = input.toCharArray();
+
+        for (char ch : stringArray) {
+            if (charCount.containsKey(ch)) {
+                charCount.put(ch, charCount.get(ch) + 1);
+            } else {
+                charCount.put(ch, 1);
+            }
+        }
+        return charCount.toString();
     }
 }
